c07eaf263c781b56810d514b5be5c73553360c12
Swapnil-101 <swapniltiwari9819@gmail.com>
2023-05-06 18:30:26 +0530

    sdad

---
+++ src/sections/banner.js
@@ -0,0 +1,93 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import { Container, Box, Heading, Text, Image, Button } from "theme-ui";
+import BannerImg from "assets/banner.jpg";
+import ShapeLeft from "assets/shape-left.png";
+import ShapeRight from "assets/shape-right.png";
+
+export default function Banner() {
+  return (
+    <section sx={styles.banner} id="home">
+      <Container sx={styles.banner.container}>
+        <Box sx={styles.banner.contentBox}>
+          <Heading as="h1" variant="heroPrimary">
+            OVERVIEW - Virtual India
+          </Heading>
+          <Text as="p" variant="heroSecondary">
+            ﻿ Ledgister Private Limited was established in 2021 with idea of
+            merging of two successful profession Legal & Accountancy. Thus,
+            Virtual India is an integrated CA & Legal service provider having
+            500+ domestic and international clients and providing broad range of
+            legal, taxation and advisory services to the commercial, industrial
+            and financial communities. We are one of very few CA & Legal service
+            provider in India having a unique combination of corporate law and
+            tax practice, and hence having good infrastructure with Pan - India
+            operations to offer all financial and legal services under one roof.
+          </Text>
+          <Button variant="primary">Explore</Button>
+        </Box>
+
+        <Box sx={styles.banner.imageBox}>
+          <Image src={"https://virtualindia.co.in/wp-content/uploads/2023/04/Untitled-design-2023-04-05T111240.572.jpg"} alt="banner" />
+        </Box>
+      </Container>
+    </section>
+  );
+}
+
+const styles = {
+  banner: {
+    pt: ["140px", "145px", "155px", "170px", null, null, "180px", "215px"],
+    pb: [2, null, 0, null, 2, 0, null, 5],
+    position: "relative",
+    zIndex: 2,
+    "&::before": {
+      position: "absolute",
+      content: '""',
+      bottom: 6,
+      left: 0,
+      height: "100%",
+      width: "100%",
+      zIndex: -1,
+      backgroundImage: `url(${ShapeLeft})`,
+      backgroundRepeat: `no-repeat`,
+      backgroundPosition: "bottom left",
+      backgroundSize: "36%",
+    },
+    "&::after": {
+      position: "absolute",
+      content: '""',
+      bottom: "40px",
+      right: 0,
+      height: "100%",
+      width: "100%",
+      zIndex: -1,
+      backgroundImage: `url(${ShapeRight})`,
+      backgroundRepeat: `no-repeat`,
+      backgroundPosition: "bottom right",
+      backgroundSize: "32%",
+    },
+    container: {
+      minHeight: "inherit",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+    },
+    contentBox: {
+      width: ["100%", "90%", "535px", null, "57%", "60%", "68%", "60%"],
+      mx: "auto",
+      textAlign: "center",
+      mb: ["40px", null, null, null, null, 7],
+    },
+    imageBox: {
+      justifyContent: "center",
+      textAlign: "center",
+      display: "inline-flex",
+      mb: [0, null, -6, null, null, "-40px", null, -3],
+      img: {
+        position: "relative",
+        height: [245, "auto"],
+      },
+    },
+  },
+};
